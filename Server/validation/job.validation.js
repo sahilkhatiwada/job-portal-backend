7abdca82877d1a75cf87b9b95dcdaa951cb6e4df
[Sahil khatiwada] <sahilkhatiwada12@gmail.com>
2024-03-08 22:06:49 +0545

    minor changes and successfully created jobs posting

--- Server/validation/job.validation.js
+++ Server/validation/job.validation.js
@@ -1,10 +1,9 @@
 import Yup from "Yup";
 
 export const jobRegSchema = Yup.object({
-  Title: Yup.string().required("jobTitle is required"),
-  company: Yup.string().required("Company is required"),
-  location: Yup.string().required("Location is required"),
-  description: Yup.string().required("Description is required"),
-  jobType: Yup.string().required("Job type is required"),
-  salary: Yup.string().required("Salary is required"),
+  jobTitle: Yup.string().required("Job title is required."),
+  companyName: Yup.string().required("Company name is required."),
+  location: Yup.string().required("Location is required."),
+  jobType: Yup.string().required("Job type is required."),
+  salary: Yup.string().required("Salary is required."),
 });
