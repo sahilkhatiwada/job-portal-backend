0c942c1e21df3ad64fc7e0affc404f85f95a78e0
[Sahil khatiwada] <sahilkhatiwada12@gmail.com>
2024-03-08 19:01:07 +0545

    recruiter login

--- Server/routes/recruiter.routes.js
+++ Server/routes/recruiter.routes.js
@@ -34,5 +34,43 @@ router.post(
 );
 
 // login router
-
+router.post("/recruiter/login", async (req, res) => {
+  const loginCredentials = req.body;
+  // find the user by email
+  const user = await Recruiter.findOne({
+    CompanyEmail: loginCredentials.email,
+  });
+  if (!user) {
+    return res.status(401).send({ message: "User not found." });
+  }
+  // compare password with the one in db
+  const isMatch = await bcrypt.compare(
+    loginCredentials.password,
+    user.password
+  );
+  if (!isMatch) {
+    return res.status(401).send({ message: "Invalid password." });
+  }
+  // send back a token for authentication
+  const token = jwt.sign(
+    { CompanyEmail: user.CompanyEmail },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "7d" }
+  );
+  res.header("auth-token", token).send(
+    jwt.decode(token, { complete: true }, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ message: "Invalid token." });
+      } else {
+        return res.json({
+          id: user._id,
+          name: `${user.FirstName} ${user.LastName}`,
+          email: user.companyEmail,
+          token: token,
+          expiry: decoded.exp,
+        });
+      }
+    })
+  );
+});
 export default router;
